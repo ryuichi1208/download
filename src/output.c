afc4b9ed963f021f78cf2ff7e23b2c5bbcbee6f3
Mark Regan <mark.regan@gmail.com>
2013-09-09 22:45:36 -0400

    Wait for key press before clearning ncurses window

--- src/output.c
+++ src/output.c
@@ -5,21 +5,43 @@
 
 static bool show_progress;
 
+//
+// output_init
+//
+// Initialize the output system.  If PROGRESS is true, enable the ncurses
+// system.
+//
 void output_init( bool progress )
 {
     show_progress = progress;
-    initscr();
-    raw();
+
+    if ( show_progress )
+    {
+        initscr();
+        raw();
+    }
 }
 
+//
+// output_end
+//
+// If necessary, close the ncurses system.
+//
 void output_end()
 {
     if ( show_progress )
     {
+        getch();
         endwin();
     }
 }
 
+//
+// output
+//
+// Print the given FORMAT and variable argument list (...), either via printf or
+// ncurses as appropriate.
+//
 void output( const char* format, ... )
 {
     va_list arglist;
